fc57ca065e7a07c18ba6b403ce72499994677380
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2022-05-31 19:09:25 +0100

    Array1.java & Array1Test.java classes added

---
+++ src/test/challenges/Array1Test.java
@@ -0,0 +1,13 @@
+package challenges;
+
+import org.junit.jupiter.api.BeforeAll;
+
+class Array1Test {
+
+    private static Array1 array1;
+
+    @BeforeAll
+    static void beforeAll() {
+        array1 = new Array1();
+    }
+}
