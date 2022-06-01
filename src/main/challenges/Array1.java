fe9fd8c8e5c7e714a26a6a5a4575fbd257ebd339
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2022-06-01 19:30:53 +0100

    sameFirstLast challenge & Test added

--- src/main/challenges/Array1.java
+++ src/main/challenges/Array1.java
@@ -17,4 +17,19 @@ public class Array1 {
         return (nums[0] == 6 || nums[nums.length - 1] == 6);
     }
 
+    /**
+     * Given an array of ints, return true if the array is length 1 or more,
+     * and the first element and the last element are equal.
+     *
+     * sameFirstLast([1, 2, 3]) → false
+     * sameFirstLast([1, 2, 3, 1]) → true
+     * sameFirstLast([1, 2, 1]) → true
+     *
+     * @param nums int[]
+     * @return True if the int[] parameter is >= 1, and the first and last elements are equal
+     */
+    public boolean sameFirstLast(int[] nums) {
+        return nums.length >= 1 && nums[0] == nums[nums.length - 1];
+    }
+
 }
--- src/test/challenges/Array1Test.java
+++ src/test/challenges/Array1Test.java
@@ -29,4 +29,19 @@ class Array1Test {
         assertFalse(firstLast6_2);
         assertTrue(firstLast6_3);
     }
+
+    @Test
+    void sameFirstLast() {
+        int[] nums = {1, 2, 3};
+        int[] nums2 = {1, 2, 1};
+        int[] nums3 = {1};
+
+        boolean sameFirstLast = array1.sameFirstLast(nums);
+        boolean sameFirstLast2 = array1.sameFirstLast(nums2);
+        boolean sameFirstLast3 = array1.sameFirstLast(nums3);
+
+        assertFalse(sameFirstLast);
+        assertTrue(sameFirstLast2);
+        assertTrue(sameFirstLast3);
+    }
 }
